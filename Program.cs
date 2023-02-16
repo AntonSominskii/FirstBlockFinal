280b13202bcb7c8756ade98171fb2b5ea53b9bf8
AntonSominski <antonsominskii@yandex.ru>
2023-02-16 23:37:01 +0300

    start array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+лучше обойтись исключительно массивами. */
+
+int ReadNumber(string message) //Функция принимает целое положительное число и преобразует его в int. 
+        {
+            Console.WriteLine(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+
+string [] StringArray(int size) //Функция принимает целое положительное число и преобразует его в int.
+        {
+           // int x = Convert.ToInt32(Console.ReadLine());
+            string[] p = new string[size];
+            for(int i = 0; i<size; i++)
+            {
+                Console.Write("Введите строку №{0}:\r\n    ", i + 1);
+                p[i] = Console.ReadLine(); //Заполняем его
+            }
+            return p;
+        }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        
+            Console.Write(array[i] + " ");
+       
+        
+    }
+}
+
+int x = ReadNumber("Введите количество элементов массива:");
+string [] startArray = StringArray(x);
+PrintArray(startArray);
